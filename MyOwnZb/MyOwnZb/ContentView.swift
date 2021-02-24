28152b28651ebe7935cf3a22e83766d6635cb2d7
OF3529 <459431498@qq.com>
2021-02-24 16:11:25 +0800

    初始化项目

---
+++ MyOwnZb/MyOwnZb/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyOwnZb
+//
+//  Created by 李敏东 on 2021/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyOwnZb/MyOwnZb/MyOwnZbApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOwnZbApp.swift
+//  MyOwnZb
+//
+//  Created by 李敏东 on 2021/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MyOwnZbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
